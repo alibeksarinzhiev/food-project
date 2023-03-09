34b3a2f42db64a94f4742c7f916247ca82637904
alibek <alibek1024@mail.ru>
2023-03-09 17:02:09 +0600

    add db

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Route ,Routes } from 'react-router-dom'; 
-
-import Header from "./Layout/Header/Header";
+import { Route ,Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home"
-
 import './App.scss'
 import Layout from './Layout/Layout';
 import SingleCard from './pages/SingleCardPage/SingleCard';
 
 const App = () => {
+
     return (
         <>
             <Routes>
+
                 <Route path='/' element={<Layout/>}>
-                    <Route path=''element={<Home/>}/>
+                    <Route path='' element={<Home/>}/>
                     <Route path='single' element={<SingleCard/>}/>
                     <Route/>
                     <Route/>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import axios from "axios";
+import Card from '../../Components/Card/Card'
+import './Home.scss'
+
 
 const Home = () => {
+    useEffect(()=>{
+        axios("http://localhost:8080/product")
+            .then(({data})=>setProduct(data))
+    },[])
+    const [product,setProduct] = useState([])
+
+
     return (
-        <section className='home'>
+        <section className='home' style={{background:'#FBF8EC'}}>
             <div className="container">
                 <div className='home__sale'>
                     <h2>Акции</h2>
                     <p>Все акции</p>
                 </div>
                 <div className='home__cards'>
-
+                    {
+                        product.map((el)=>(
+                            <Card el={el}/>
+                        ))
+                    }
                 </div>
             </div>
 
